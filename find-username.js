9ae7e047736b4c90f12ab3f64cba4cb34bb5573b
Darryl Tanzil <darryltanzil@gmail.com>
2021-12-22 13:17:30 -0800

    init

---
+++ find-username.js
@@ -0,0 +1,7 @@
+var username = prompt("Enter in the username you wish to determine.")
+
+const data = require('instagram.com/' + username + '/?__a=1');
+console.log(data);
+
+
+
---
+++ not-following-back.js
@@ -0,0 +1,4 @@
+var arr1 = [1,2,3,4]
+    arr2 = [2,4],
+    res = arr1.filter(item => !arr2.includes(item));
+console.log(res);
