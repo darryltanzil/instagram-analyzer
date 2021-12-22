3d7f1d2870ce3029fa5437ef3a9a4ceb943bcad8
Darryl Tanzil <darryltanzil@gmail.com>
2021-12-22 15:20:00 -0800

    removed prompt-sync in favor of using web app

--- get-username-data.js
+++ get-username-data.js
@@ -4,9 +4,10 @@ var username = prompt("Enter in the username you wish to determine:")
 // fetch JSON Data
 var request = require('request');
 
-request('https://www.instagram.com/' + username + '/?__a=1', function (error, response, body) {
+request('https://www.instagram.com/darryltanzil/?__a=1', function (error, response, body) {
   if (!error && response.statusCode == 200) {
-     var importedJSON = JSON.parse(body);
+    console.log(body);
+     /* var importedJSON = JSON.parse(body);
      console.log(importedJSON);
 
      // get user id by fetching logging page id and filtering to only numbers
@@ -15,7 +16,7 @@ request('https://www.instagram.com/' + username + '/?__a=1', function (error, re
 
      //var followingList = getFollowList(1, user_id);
      //var followerList = getFollowList(2, user_id);
-     
+     */
   }
   else {
     console.log("Username not found.");
